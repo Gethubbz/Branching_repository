64c3e4e35e4a9d41fbea9bf6e34c0904034f5cac
Chase Parker <Chaseparker2533@gmail.com>
2025-09-25 20:04:59 +0000

    git add chaseparker_assignment_3.py git commit -m "Pass Test Case 1: Initial game setup with required variables"

---
+++ chaseparker_assignment_3.py
@@ -0,0 +1,14 @@
+student_name = "Chase"
+current_gpa = 3.5
+study_hours =  19
+social_points = 67
+stress_level = 89
+user_choice=["Programming", "Math", "English", "History"]
+print(f"Welcome {student_name}, your GPA is {current_gpa}, with {study_hours} study hours, {social_points} social points, and  a stress level of {stress_level}")
+study_options=["Programming", "Math", "English", "History"]
+print("Choose your course load:")
+print("A) Light (12 credits)") 
+print("B) Standard (15 credits)")
+print("C) Heavy (18 credits)")
+
+
