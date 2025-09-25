18ba3933d3ac0667cf0bea23db12602c3b0fac8a
Chase Parker <Chaseparker2533@gmail.com>
2025-09-25 20:18:13 +0000

    git add chaseparker_assignment_3.py git commit -m "Pass Test Case 2: Course planning with if/elif/else and comparison operators"

--- chaseparker_assignment_3.py
+++ chaseparker_assignment_3.py
@@ -12,3 +12,39 @@ print("B) Standard (15 credits)")
 print("C) Heavy (18 credits)")
 
 
+choice= str(input())
+study_choice=str(input())
+choice_text=''
+if choice == "A":
+    if  study_hours<stress_level:
+      study_hours-=10
+      stress_level-=40
+      choice_text="A) Light (12 credits)"
+
+    else:
+      study_hours-=4
+      stress_level-=20
+      choice_text="A) Light (12 credits)"
+elif choice == "B":
+  
+ 
+  study_hours+=3
+  stress_level+=5
+  choice_text="B) Standard (15 credits)"
+elif choice == "C":
+  
+    if current_gpa>=3.5:
+        study_hours+=4
+        stress_level+=10
+        choice_text="C) Heavy (18 credits)"
+
+    else:
+        study_hours+=3
+        stress_level+=5
+        choice_text="C) Heavy (18 credits)"
+else:
+      choice_text="ERROR"
+   
+
+
+
